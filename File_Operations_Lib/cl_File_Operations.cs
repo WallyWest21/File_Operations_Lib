099b86f477e595b1eb46f18e80a16f82d067ba1c
WallyWest21 <WallyWest21@users.noreply.github.com>
2018-02-06 19:14:50 -0500

    first commit

---
+++ File_Operations_Lib/cl_File_Operations.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Operations_Lib
+{
+    public class cl_File_Operations
+    {
+        public List<string> SelectFilesToRead()
+        {
+            List<string> MySelectedFilesToRead = new List<string>();
+            System.Windows.Forms.OpenFileDialog MyFileDiaog = new System.Windows.Forms.OpenFileDialog();
+
+            MyFileDiaog.InitialDirectory = @"C:\";
+            MyFileDiaog.Title = "Browse Text Files";
+
+            MyFileDiaog.CheckFileExists = true;
+            MyFileDiaog.CheckPathExists = true;
+
+            MyFileDiaog.DefaultExt = "txt";
+            MyFileDiaog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            //MyFileDiaog.FilterIndex = 2;
+            MyFileDiaog.RestoreDirectory = true;
+
+            MyFileDiaog.ReadOnlyChecked = true;
+            MyFileDiaog.ShowReadOnly = true;
+            MyFileDiaog.Multiselect = true;
+
+
+
+            DialogResult result = MyFileDiaog.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                foreach (String file in MyFileDiaog.FileNames)
+                {
+                    MySelectedFilesToRead.Add(file);
+                }
+            }
+            return MySelectedFilesToRead;
+        }
+
+        public string SelectFolder()
+        {
+            System.Windows.Forms.FolderBrowserDialog MyFolderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+
+            DialogResult result = MyFolderBrowserDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                return MyFolderBrowserDialog.SelectedPath;
+            }
+            return null;
+        }
+
+
+        public void CreateFolder(string FolderPathString)
+        {
+            if (!System.IO.Directory.Exists(FolderPathString))
+            {
+                System.IO.Directory.CreateDirectory(FolderPathString);
+            }
+            else
+            { MessageBox.Show("Folder: " + FolderPathString + " already exists!"); }
+
+
+        }
+        public void CreateTxtFile()
+        {
+
+        }
+
+        public void DeleteFolder(string FolderPathString)
+        {
+            if (System.IO.Directory.Exists(FolderPathString))
+            {
+                System.IO.Directory.Delete(FolderPathString);
+            }
+            else
+            { MessageBox.Show("Folder: " + FolderPathString + " doesn't exists!"); }
+
+        }
+
+        public void DeleteFile(string FilePathString)
+        {
+            if (System.IO.File.Exists(FilePathString))
+            {
+                System.IO.File.Delete(FilePathString);
+            }
+            else
+            { MessageBox.Show("File: " + FilePathString + " doesn't exists!"); }
+
+        }
+
+        public void CopyAndPasteFile(string SourceFileName, string SourcePath, string TargetPath, string TargetFileName="", bool OverWriteFile=false)
+        {
+            string SourceFilePathString = SourcePath + @"\" + SourceFileName;
+            if (TargetFileName=="") { TargetFileName = SourceFileName; }
+            string TargetFilePathString = TargetPath + @"\" + TargetFileName;
+
+            if (!System.IO.Directory.Exists(TargetPath))
+            { CreateFolder(TargetPath); }
+
+                if (System.IO.File.Exists(SourceFilePathString))
+            {
+                System.IO.File.Copy(SourceFilePathString, TargetFilePathString, OverWriteFile);
+            }
+            else
+            { MessageBox.Show("File: " + SourceFilePathString + " doesn't exists!"); }
+
+        }
+        public void CutAndPasteFile(string SourceFileName, string SourcePath, string TargetPath, string TargetFileName = "", bool OverWriteFile = false)
+        {
+            string SourceFilePathString = SourcePath + @"\" + SourceFileName;
+            if (TargetFileName == "") { TargetFileName = SourceFileName; }
+            string TargetFilePathString = TargetPath + @"\" + TargetFileName;
+
+            if (!System.IO.Directory.Exists(TargetPath))
+            { CreateFolder(TargetPath); }
+
+            if (System.IO.File.Exists(SourceFilePathString))
+            {
+                System.IO.File.Move(SourceFilePathString, TargetFilePathString);
+            }
+            else
+            { MessageBox.Show("File: " + SourceFilePathString + " doesn't exists!"); }
+
+
+        }
+
+        public void CopyAndPasteFolder(string SourcePath, string TargetPath, bool OverWriteFile = false)
+        {
+            if (System.IO.Directory.Exists(SourcePath))
+            {
+                System.IO.Directory.Move(SourcePath, TargetPath);
+            }
+            else
+            { MessageBox.Show("Folder: " + SourcePath + " doesn't exists!"); }
+        }
+        public void CutAndPasteFolder(string FilePathString)
+        {
+            if (System.IO.File.Exists(FilePathString))
+            {
+
+            }
+            else
+            { MessageBox.Show("File: " + FilePathString + " doesn't exists!"); }
+
+        }
+
+
+        bool ItIsAFile(string FilePathString)
+        {
+
+            return false;
+        }
+
+        bool ItIsAFolder()
+        {
+            return false;
+        }
+
+        bool ItIsACertainFile(string FileExtension)
+        {
+            return false;
+        }
+
+        double FileSize()
+        {
+            return 0;
+        }
+
+        void WriteListToTextFile(List<object> MyList)
+        {
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter(@"c:\temp\important.txt", true))
+            {
+                foreach (object element in MyList)
+                {
+                    string line = null;
+                    foreach (System.Reflection.PropertyInfo propertyInfo in element.GetType().GetProperties()) //https://stackoverflow.com/questions/9893028/c-sharp-foreach-property-in-object-is-there-a-simple-way-of-doing-this
+                    {line = line + propertyInfo.GetValue(element).ToString() + ", ";}
+                    writer.WriteLine(line.Substring(0, line.Length - 2));
+                    //Console.WriteLine(line.Substring(0, line.Length - 2));
+                }
+            }
+        }
+
+
+    }
+}
---
+++ test_FileOperations/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace test_FileOperations
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        File_Operations_Lib.cl_File_Operations cl_IO = new File_Operations_Lib.cl_File_Operations();
+        List<user> MyList = new List<user>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyList.Add(new user {ID=1, Description="bmjg", Name="Ray Ray"});
+            MyList.Add(new user { ID = 2, Description = "vbxgh", Name = "Pookie" });
+            MyList.Add(new user { ID = 3, Description = "nvbnnv", Name = "Jerome" });
+            MyList.Add(new user { ID = 4, Description = "bvnvbnvnvbn", Name = "Dewayne" });
+            MyList.Add(new user { ID = 5, Description = "dsggncgfd", Name = "Tyrone" });
+
+            var MyNewlist =( from m in MyList select m).ToList<object>();
+            WriteListToTextFile(MyNewlist);
+
+            //foreach (user element in MyList)
+            //{
+            //    string line = null;
+            //     StringBuilder sb = new StringBuilder(); //https://msdn.microsoft.com/en-us/library/hdekwk0b(v=vs.110).aspx?cs-save-lang=1&cs-lang=csharp#code-snippet-2
+            //    foreach (PropertyInfo propertyInfo in element.GetType().GetProperties()) //https://stackoverflow.com/questions/9893028/c-sharp-foreach-property-in-object-is-there-a-simple-way-of-doing-this
+            //    {
+            //        line =line+ propertyInfo.GetValue(element).ToString()+", ";
+            //        //sb.AppendFormat("{0},", propertyInfo.GetValue(element));
+
+            //        //Console.WriteLine("{0}, {1}",propertyInfo.Name,propertyInfo.GetValue(element));
+            //        //Console.WriteLine(propertyInfo.GetValue(propertyInfo.Name).ToString());
+
+            //        //propertyInfo.Name;
+            //        // do stuff here
+            //    }
+            //    //Console.WriteLine(propertyInfo.GetValue(propertyInfo.Name).ToString());
+            //    Console.WriteLine(line.Substring(0,line.Length - 2));
+
+            //}
+
+
+        }
+
+        void WriteListToTextFile(List <object> MyList)
+        {
+            using (StreamWriter writer = new StreamWriter(@"c:\temp\important.txt", true))
+            {
+                foreach (object element in MyList)
+                {
+                    string line = null;
+                    foreach (PropertyInfo propertyInfo in element.GetType().GetProperties()) //https://stackoverflow.com/questions/9893028/c-sharp-foreach-property-in-object-is-there-a-simple-way-of-doing-this
+                    {line = line + propertyInfo.GetValue(element).ToString() + ", ";}
+                    writer.WriteLine(line.Substring(0, line.Length - 2));
+                    Console.WriteLine(line.Substring(0, line.Length - 2));
+                }
+            }
+        }
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            foreach (string file in cl_IO.SelectFilesToRead())
+            {
+                MessageBox.Show(file);
+            }
+        }
+
+        class user
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+        }
+    }
+}
